bc55e00a1988d71d7d69bf1c2763b2b51a2a8d2c
gitdrik <58667795+gitdrik@users.noreply.github.com>
2025-12-12 15:48:50 +0100

    Add files via upload

---
+++ 12.jl
@@ -0,0 +1,18 @@
+open("12.txt") do f
+    ans = 0
+    for (i, l) ∈ enumerate(eachline(f))
+        i < 31 && continue
+        rows, cols, ns... = parse.(Int, split(l, r"x|: | "))
+        if rows÷3 * cols÷3 ≥ sum(ns)
+            # Yes, will work.
+            ans +=1
+        elseif sum(ns[1:4])*7 + ns[5]*5 + ns[6]*6 > rows*cols
+            # No, will not work.
+            nothing
+        else
+            println("$i: Undefined, method not implemented.")
+            exit()
+        end
+    end
+    println("Answer: ", ans)
+end
