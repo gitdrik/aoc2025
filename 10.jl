e86d632327862252f6b45c62585a6a31f3857aa4
gitdrik <58667795+gitdrik@users.noreply.github.com>
2025-12-20 22:38:08 +0100

    Add files via upload

--- 10.jl
+++ 10.jl
@@ -1,8 +1,7 @@
-using DataStructures
-
+function run()
 open("10.txt") do f
     p1, p2 = 0, 0
-    for (i, l) ∈ enumerate(eachline(f))
+    for l ∈ eachline(f)
         ss = split(l)
         goal = collect(ss[1][2:end-1]) .== '#'
         buttons::Array{BitArray} = []
@@ -13,50 +12,39 @@ open("10.txt") do f
             end
             push!(buttons, b)
         end
-        
-        seen::Set{typeof(goal)} = Set()
-        Q = [(0, falses(length(goal)))]
-        while true
-            presses, state = popfirst!(Q)
-            state ∈ seen && continue
-            push!(seen, state)
-            if state == goal
-                p1 += presses
-                break
-            end
-            for b ∈ buttons
-                nstate = state .⊻ b
-                push!(Q, (presses+1, nstate))
-            end
-        end
 
-        joltages = parse.(Int, split(ss[end][2:end-1], ','))
-        goal == zeros(Int, length(joltages))
-        seen2::Set{typeof(joltages)} = Set()
-        Q = SortedSet([(maximum(joltages), 0, joltages)])
-        while true
-            _e, presses, state = popfirst!(Q)
-#            @show _e, presses, state
-            state ∈ seen2 && continue
-            any(state .< 0) && continue
-            push!(seen2, state)
-            if state == goal
-                println(presses)
-                p2 += presses
-                break
-            end
-            for b ∈ buttons
-                nstate = state .- b
-                push!(Q, (maximum(nstate)+presses+1, presses+1, nstate))
+        bn = length(buttons)
+        ans = bn
+        for n ∈ 1:2^bn-1
+            press = digits(n, base=2, pad=bn).==1
+            if isodd.(sum(buttons[press])) == goal
+                ans = min(ans, sum(press))
             end
         end
-        @show p2, i, l
+        p1 += ans
+
+        # This solution is based on others ideas.
+        # My original one was an ugly combination of computing
+        # Smiths normal form using NormalForms.jl, and brute force.
+        jolts = parse.(Int, split(ss[end][2:end-1], ','))
+        MEM::Dict{Array{Int}, Int} = Dict()
+        function minpress(jolts)
+            all(jolts.==0) && return 0
+            jolts ∈ keys(MEM) && return MEM[jolts]
+            ans = 100000
+            for n ∈ 0:2^bn-1
+                press = digits(n, base=2, pad=bn).==1
+                nj = n==0 ? jolts : jolts .- sum(buttons[press])
+                any(nj .< 0) && continue
+                any(isodd.(nj)) && continue
+                ans = min(ans, sum(press)+2*minpress(nj.÷2))
+            end
+            MEM[jolts] = ans
+            return ans
+        end
+        p2 += minpress(jolts)
     end
     println("Part 1: ", p1)
     println("Part 2: ", p2)
-
 end
-            
-            
-
-#[.##.##] (4,5) (0,5) (2,3) (1,3,5) (0,3,5) (0,2,3,5) (0,1,4) (0,2,4,5) {198,181,22,50,173,65}
+end
